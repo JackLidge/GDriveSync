c40e727f48da3a9db44571d20ce011b8492c78a6
JackLidge <jrglmusic@gmail.com>
2020-01-06 12:15:11 +0000

    Added in capability to delete files after archived length of time. Removed delete_items_in_gdrive_folder.py and upload_handler.py as their functions have been put into upload.py

--- upload.py
+++ upload.py
@@ -1,6 +1,8 @@
+#!/opt/miniconda3/envs/mdc1_7/bin/python3
 import os
 import logging
 import datetime
+import click
 from pydrive.drive import GoogleDrive 
 from pydrive.auth import GoogleAuth
 
@@ -33,7 +35,7 @@ def log2File(filename, text):
         f.write(f"{ftime} | {text}")
     return
 
-def gdrive_sync(gauth, os_root_path, gdrive_root_folder_id):
+def gdrive_sync(gauth, os_root_path, gdrive_root_folder_id, archive_time):
     '''
     Function which performs the sync operation.
     TODO: Add in function which deletes files from Google Drive which are no longer present in
@@ -42,15 +44,19 @@ def gdrive_sync(gauth, os_root_path, gdrive_root_folder_id):
 
     with open('update_log.txt') as f:
         log_info = f.readlines()
-        last_line = log_info[-1].strip()
-        log_mod_time = last_line.split('|')
-        log_mod_time = datetime.datetime.strptime(log_mod_time[0], '%Y-%m-%d %H:%M:%S ')
-    
+        try:
+            last_line = log_info[-1].strip()
+            log_mod_time = last_line.split('|')
+            log_mod_time = datetime.datetime.strptime(log_mod_time[0], '%Y-%m-%d %H:%M:%S ')
+        except IndexError:
+            log_mod_time = datetime.datetime(1900, 1, 1)
+    print(log_mod_time)
     drive = GoogleDrive(gauth)
     dir_dict = {os_root_path: gdrive_root_folder_id}
-    item_list = drive.ListFile({'q': f'"{folder_id}" in parents and trashed=false'}).GetList()
+    item_list = drive.ListFile({'q': f'"{gdrive_root_folder_id}" in parents and trashed=false'}).GetList()
     
     for dirpath, dirnames, files in os.walk(os_root_path):
+        print(dirnames)
         for directory in dirnames:
             dir_mod_time = os.path.getmtime(os.path.join(dirpath, directory))
             dir_mod_time = datetime.datetime.fromtimestamp(dir_mod_time)
@@ -65,11 +71,11 @@ def gdrive_sync(gauth, os_root_path, gdrive_root_folder_id):
         for file in files:
             file_mod_time = os.path.getmtime(os.path.join(dirpath, file))
             file_mod_time = datetime.datetime.fromtimestamp(file_mod_time)
-            
+            print(file_mod_time)
             # If the file in the local directory was modified more recently than the last backup
             # was run, upload to Google Drive.
             if file_mod_time > log_mod_time:
-
+                print('files should be updated')
                 new_file = drive.CreateFile({'title': file, 
                                              'parents': [{'kind': 'drive#fileLink', 
                                                           'id': dir_dict[dirpath]}]})
@@ -81,8 +87,17 @@ def gdrive_sync(gauth, os_root_path, gdrive_root_folder_id):
                 #Upload new file.
                 new_file.SetContentFile(os.path.join(dirpath, file))
                 new_file.Upload()
+        
+        # If option is selected, deletes all files older than a certain date.        
+        if archive_time != -1:
+            cur_date = datetime.datetime.now()
+            for file in item_list:
+                mod_date = file['modifiedDate']
+                mod_date = datetime.datetime.strptime(mod_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+                if int((cur_date - mod_date).days) > int(archive_time):
+                    file.Delete()
 
-def upload(path, folder_id):
+def upload(path, folder_id, archive):
 
     """ Initial Authentication, local """
     gauth = GoogleAuth()
@@ -96,8 +111,7 @@ def upload(path, folder_id):
         gauth.Authorize()
     gauth.SaveCredentialsFile('credentials.txt')
 
-	# Empty Gdrive folder
-    gdrive_sync(gauth, path, folder_id)
+    gdrive_sync(gauth, path, folder_id, archive)
     log2File('update_log.txt', f'Synced with GDrive\n')
 
 
@@ -106,10 +120,15 @@ def upload(path, folder_id):
 	the upload call below this section
 """
 
-import csv
-with open('folder_sync_registrer.txt', 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        folder_id = row[1]
-        root_path = row[0]
-        upload(row[0], row[1])
+@click.command()
+@click.option('--archive', default=-1, help='Time length of data to be archived.')
+
+def main(archive):
+    with open('folder_sync_registrer.txt', 'r') as f:
+        info = f.readlines()
+        info = info[0].split(',')
+        info = [i.strip() for i in info]
+        upload(info[0], info[1], archive)
+        
+if __name__ == '__main__':
+    main()
